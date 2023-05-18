5b26c4b2c3e6d46877676d1aa5d71b7ada22f233
Lucas Cein <lucascein82@gmail.com>
2023-05-18 16:04:26 -0300

    actualizacion1

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -8,7 +8,7 @@ const ItemList = ({ productos }) => {
             <Row xs={1} md={4} className="ms-2 me-2 mt-2">
             {
                 productos.map(({id, price, picture, category, Title}) => 
-                    <Col className="mb-2">
+                    <Col key={id} className="mb-2">
                         <Item key={id} id={id} price={price} picture={picture} category={category} Title={Title}   />
                     </Col>
                 )
